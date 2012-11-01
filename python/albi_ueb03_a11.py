1d79ff3e1e65a27d9139e5cd26ec50be2cf08d9c
hielsnoppe <accounts@nielshoppe.de>
2012-11-01 12:12:26 +0100

    Übung 3, Aufgabe 11 Eingabe, Ausgabe und UPGMA Algorithmus verbessert.

--- python/albi_ueb03_a11.py
+++ python/albi_ueb03_a11.py
@@ -3,6 +3,7 @@ import os
 import re
 import itertools
 import heapq
+import argparse
 
 def dist(seqa, seqb):
 	""" returns normalized hamming distance of the sequences seqa and seqb """
@@ -18,43 +19,62 @@ def jcest(dist, len):
 		return 0.0
 
 def read_alignment(filename):
-	with open(filename, 'r') as seqfile:
+	with open(filename, 'r') as f:
 		seq = {}
 		regex = r"^>(\S+)\s"
 		name = ""
-		for line in seqfile:
+		for line in f:
 			c = re.match(regex, line)
 			if (c):
-				name = c.group(1)
-				seq[name] = ""
+				name = c.group(1)			# get name for next sequence
+				seq[name] = ""				# create empty entry for next sequence
 			else:
-				seq[name] += line.strip()
+				seq[name] += line.strip()	# add line to current sequence
 	return seq
 
 def create_distance_matrix(alignment):
 	return [[dist(seqa, seqb) for seqb in alignment] for seqa in alignment]
 
 def upgma(alignment, d):
-	indices = [i for i in range(len(d))]
 	clusters = dict([(i, i) for i in range(len(d))])
-	next = len(indices)
-	while len(indices) > 1:
-		candidates = [(d[a][b], a, b) for a, b in itertools.combinations(indices, 2)]
+	next = len(clusters)
+	while len(clusters) > 1:
+		candidates = [(d[a][b], a, b) for a, b in itertools.combinations(clusters.keys(), 2)]
 		heapq.heapify(candidates)
 		dist, a, b = heapq.heappop(candidates)
 		for row in d:
 			row.append((row[a] + row[b]) / 2)		# compute entry for new column
 		d.append([row[next] for row in d] + [0])	# copy last column to new row
-		indices.remove(a)
-		indices.remove(b)
-		indices.append(next)
 		clusters[next] = (clusters[a], clusters[b])
+		del(clusters[a])
+		del(clusters[b])
 		next += 1
 	return clusters[next-1]
 
-alignment = read_alignment("../data/test.aln")
-#alignment = {"1":"ACG--TA", "2":"ACAGGTA", "3":"ACGA-TA", "4":"CC-GGTA"}
 
-d = create_distance_matrix(alignment)
-tree = upgma(alignment, d)
-print (tree)
+
+parser = argparse.ArgumentParser(description="Uebung 3 - Aufgabe 11")
+
+parser.add_argument('-i', '--input',
+type=str,
+required=True,
+help='Eingabedatei')
+
+parser.add_argument('-o', '--output',
+type=str,
+required=False,
+help='Ausgabedatei')
+
+args = parser.parse_args()
+infile = args.input
+outfile = args.output
+
+a = read_alignment(infile)
+d = create_distance_matrix(a)
+tree = upgma(a, d)
+
+if outfile:
+	with open(outfile, 'w') as f:
+		f.write(str(tree))
+else:
+	print(tree)
